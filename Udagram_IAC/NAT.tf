d45910d4f1cdae25b19c0a01efe499e7c336919b
IB-IBRAHEEM-EJALONIBU <ibejalon@gmail.com>
2020-04-22 22:20:10 -0700

    Update to NAT

--- Udagram_IAC/NAT.tf
+++ Udagram_IAC/NAT.tf
@@ -9,18 +9,12 @@ resource "aws_nat_gateway" "nat-gateway" {
   depends_on    = [aws_internet_gateway.udagram-gw]
 }
 
-resource "aws_nat_gateway" "nat-gateway" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.udagram-public-2.id
-  depends_on    = [aws_internet_gateway.udagram-gw]
-}
-
 # VPC setup for NAT
 resource "aws_route_table" "udagram-private" {
   vpc_id = aws_vpc.udagram.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.udagram-gw.id
+    nat_gateway_id = aws_nat_gateway.nat-gateway.id
   }
 
   tags = {
