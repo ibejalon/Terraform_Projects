fde8a32c4b026712adb375602cbd4ef7f5940e3c
IB-IBRAHEEM-EJALONIBU <ibejalon@gmail.com>
2020-04-26 17:53:33 -0700

    key update

--- Udagram/vars.tf
+++ Udagram/vars.tf
@@ -11,7 +11,7 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-2 = "ami-0fc20dd1da406780b"
+    us-east-2 = "ami-07c1207a9d40bc3bd"
     us-west-1 = "ami-03ba3948f6c37a4b0"
     us-west-2 = "ami-0d1cd67c26f5fca19"
   }
