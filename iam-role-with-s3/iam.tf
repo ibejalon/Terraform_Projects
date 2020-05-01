3d5cd55172fd60ee640f7eb702c2bd0f41214284
ibejalon <34724841+ibejalon@users.noreply.github.com>
2020-04-30 21:05:24 -0700

    Update iam.tf

--- iam-role-with-s3/iam.tf
+++ iam-role-with-s3/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "s3-azbucket-role" {
-  name               = "s3-azbucket-role"
+resource "aws_iam_role" "s3-ib-bucket-role" {
+  name               = "s3-ib-bucket-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -18,14 +18,14 @@ EOF
 
 }
 
-resource "aws_iam_instance_profile" "s3-azbucket-role-instanceprofile" {
-  name = "s3-azbucket-role"
-  role = aws_iam_role.s3-azbucket-role.name
+resource "aws_iam_instance_profile" "s3-ib-bucket-role-instanceprofile" {
+  name = "s3-ib-bucket-role"
+  role = aws_iam_role.s3-ib-bucket-role.name
 }
 
-resource "aws_iam_role_policy" "s3-azbucket-role-policy" {
-  name = "s3-azbucket-role-policy"
-  role = aws_iam_role.s3-azbucket-role.id
+resource "aws_iam_role_policy" "s3-ib-bucket-role-policy" {
+  name = "s3-ib-bucket-role-policy"
+  role = aws_iam_role.s3-ib-bucket-role.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -36,8 +36,8 @@ resource "aws_iam_role_policy" "s3-azbucket-role-policy" {
               "s3:*"
             ],
             "Resource": [
-              "arn:aws:s3:::azbucket-c29df1",
-              "arn:aws:s3:::azbucket-c29df1/*"
+              "arn:aws:s3:::ib-bucket-c29df1",
+              "arn:aws:s3:::ib-bucket-c29df1/*"
             ]
         }
     ]
