c3278c515518b44503bb9525a3ac71cdaacaf776
IB-IBRAHEEM-EJALONIBU <ibejalon@gmail.com>
2020-05-12 14:05:24 -0700

    updated vars file

--- Udagram/vars.tf
+++ Udagram/vars.tf
@@ -12,8 +12,6 @@ variable "AMIS" {
   type = map(string)
   default = {
     us-east-2 = "ami-07c1207a9d40bc3bd"
-    us-west-1 = "ami-03ba3948f6c37a4b0"
-    us-west-2 = "ami-0d1cd67c26f5fca19"
   }
 }
 
